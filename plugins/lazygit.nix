ae2846c35df4c323602eea5457505b9069f76993
John Wilger <john@johnwilger.com>
2024-06-15 09:13:18 -0700

    Adds LazyGit keymap as <leader>gg

--- plugins/lazygit.nix
+++ plugins/lazygit.nix
@@ -1,5 +1,15 @@
 { ...}:
 {
+  rootOpts = {
+    keymaps = [
+      {
+        action = "<cmd>LazyGit<CR>";
+        mode = "n";
+        key = "<leader>gg";
+        options.desc = "Open LazyGit window";
+      }
+    ];
+  };
   opts = {
     enable = true;
   };
